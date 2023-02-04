9c36534e91054d91b091a7a6999b44048479a9df
analizatorius <analizatoriuslx@yahoo.co.uk>
2023-02-05 00:55:19 +0200

    recourse spawnder added with bugs

---
+++ Assets/FX/Recourses_float/RecourseSpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecourseSpawner : MonoBehaviour
+{
+    private int ecourseCount = 6;
+    private string recourseTag = "leaf";
+    [SerializeField]private Vector3 spawnPos;
+    [SerializeField] private GameObject recourse;
+    [SerializeField] private Material[] mats;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space")){
+            Create();
+        }
+    }
+
+    void Create(){
+        GameObject newObject = Instantiate(recourse) as GameObject;
+        var getFloatToTarget = newObject.GetComponent<FloatToTarget>();
+        foreach(Material mat in mats){
+            Debug.Log(mat.name);
+            if (mat.name == $"{recourseTag}_mat")
+                getFloatToTarget.material = mat;
+        }
+        getFloatToTarget.recourseCount = ecourseCount;
+        getFloatToTarget.recourseTag = recourseTag;
+        getFloatToTarget.spawnPos = spawnPos;
+    }
+}
