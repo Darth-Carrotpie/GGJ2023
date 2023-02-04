6f9ceeb330047cb01ff39dd6b9a45a8593aff71f
BorbJox <jokubaskr@gmail.com>
2023-02-04 22:32:52 +0200

    merge main into main

--- Assets/Scenes/TestKS/LJack/LJack.cs
+++ Assets/Scenes/TestKS/LJack/LJack.cs
@@ -9,6 +9,11 @@ public class LJack : MonoBehaviour
 
     private class ActionRef { }
     private ActionRef _actionRef;
+    public AudioClip entryClip;
+    public AudioClip damageClip;
+
+    private AudioSource _soundSource;
+
 
     public UnityEngine.Events.UnityEvent onSwing;
     public float swingInterval;
@@ -16,6 +21,8 @@ public class LJack : MonoBehaviour
     void Awake()
     {
         _animator = GetComponent<Animator>();
+        _soundSource = this.gameObject.AddComponent<AudioSource>();
+        _soundSource.PlayOneShot(entryClip);
     }
 
     public IEnumerator Walk(Vector3 from, Vector3 to, float duration)
@@ -50,11 +57,16 @@ public class LJack : MonoBehaviour
 
         _animator.Play("spin");
 
+        _soundSource.clip = damageClip;
+        _soundSource.loop = true;
+        _soundSource.Play();
+
         while (true)
         {
             if (_actionRef != actionRef)
             {
                 Debug.Log("Spin animation interrupted");
+                _soundSource.Stop();
                 yield break;
             }
 
