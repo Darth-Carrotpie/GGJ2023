34640031269c0729659fc96653e650b333e84183
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2023-02-04 16:14:31 +0200

    LJack animation done

---
+++ Assets/Scenes/TestKS/LJack/LJack.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class LJack : MonoBehaviour
+{
+  private Animator _animator;
+
+  private class ActionRef { }
+  private ActionRef _actionRef;
+
+  void Awake()
+  {
+    _animator = GetComponent<Animator>();
+  }
+
+  public IEnumerator Walk(Vector3 from, Vector3 to, float duration)
+  {
+    var actionRef = new ActionRef();
+    _actionRef = actionRef;
+
+    _animator.Play("walk");
+    float startTime = Time.time;
+
+    while (Time.time < startTime + duration)
+    {
+      if (_actionRef != actionRef)
+      {
+        Debug.Log("Walk animation interrupted");
+        yield break;
+      }
+
+      float t = (Time.time - startTime) / duration;
+      transform.position = Vector2.Lerp(from, to, t);
+
+      yield return null;
+    }
+
+    transform.position = to;
+  }
+
+  public IEnumerator Spin(Vector3 peeSpot, float peeIntensity)
+  {
+    var actionRef = new ActionRef();
+    _actionRef = actionRef;
+
+    _animator.Play("spin");
+
+    while (true)
+    {
+      if (_actionRef != actionRef)
+      {
+        Debug.Log("Spin animation interrupted");
+        yield break;
+      }
+
+      yield return null;
+    }
+  }
+
+  public IEnumerator Leave(Vector3 from, Vector3 to, float duration)
+  {
+    var actionRef = new ActionRef();
+    _actionRef = actionRef;
+
+    _animator.Play("leave");
+    float startTime = Time.time;
+
+    while (Time.time < startTime + duration)
+    {
+      if (_actionRef != actionRef)
+      {
+        Debug.Log("Leave animation interrupted");
+        yield break;
+      }
+
+      // TODO: animate jump
+      float t = (Time.time - startTime) / duration;
+      transform.position = Vector2.Lerp(from, to, t);
+
+      yield return null;
+    }
+
+    transform.position = to;
+  }
+
+  public void Stop()
+  {
+    _actionRef = null;
+  }
+
+  public void PutDown()
+  {
+    _actionRef = null;
+    Destroy(gameObject);
+  }
+}
--- Assets/Scenes/TestKS/TestKS.cs
+++ Assets/Scenes/TestKS/TestKS.cs
@@ -8,16 +8,21 @@ public class TestKS : MonoBehaviour
   public Transform dogSpawn;
   public Transform peeSpot;
 
+  public LJack lJackPrefab;
+  public Transform lJackSpawn;
+  public Transform cutSpot;
+
   void Start()
   {
-    StartCoroutine(Test());
+    StartCoroutine(TestDog());
+    StartCoroutine(TestLJack());
   }
 
-  IEnumerator Test()
+  IEnumerator TestDog()
   {
     var dog = Instantiate<Dog>(dogPrefab);
 
-    yield return StartCoroutine(dog.Walk(dogSpawn.position, peeSpot.position, 3));
+    yield return StartCoroutine(dog.Walk(dogSpawn.position, peeSpot.position, 5));
 
     StartCoroutine(dog.Pee(peeSpot.position, 100));
 
@@ -25,8 +30,26 @@ public class TestKS : MonoBehaviour
     // while (!clicked) { yield return null; }
     yield return new WaitForSeconds(3);
 
-    yield return StartCoroutine(dog.Fetch(peeSpot.position, dogSpawn.position, 0.5f));
+    yield return StartCoroutine(dog.Fetch(peeSpot.position, dogSpawn.position, 1f));
 
     dog.PutDown();
+  }
+
+  IEnumerator TestLJack()
+  {
+    var lJack = Instantiate<LJack>(lJackPrefab);
+    lJack.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+    yield return StartCoroutine(lJack.Walk(lJackSpawn.position, cutSpot.position, 5));
+
+    StartCoroutine(lJack.Spin(cutSpot.position, 100));
+
+    // potential implementation to respond
+    // while (!clicked) { yield return null; }
+    yield return new WaitForSeconds(3);
+
+    yield return StartCoroutine(lJack.Leave(cutSpot.position, lJackSpawn.position, 3f));
+
+    lJack.PutDown();
   }
 }
