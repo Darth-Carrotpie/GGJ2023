d38e10dc90869d143ba59074ad9770a73490b853
analizatorius <analizatoriuslx@yahoo.co.uk>
2023-02-04 20:05:41 +0200

    Swipe and spawn sticks, rain fixed

--- Assets/Scripts/Stick Throw/StickFlick.cs
+++ Assets/Scripts/Stick Throw/StickFlick.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,33 +7,32 @@ public class StickFlick : MonoBehaviour
 {
     
 
-    [SerializeField] private GameObject stick;
+    //[SerializeField] private GameObject stick;
     [SerializeField] private GameObject anchorForRot;
     [SerializeField] private float rotSpeed = 100f;
     [SerializeField] private float arcSpeed = 20f;
     [SerializeField] private float speed = 20f;
     [SerializeField] private int destoryAfter = 6;
 
-    int randomNumber;
+    public string direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        float randomNumber = Random.Range(0, 10);
-        Debug.Log(randomNumber);
-        Destroy(stick, destoryAfter);
+        anchorForRot = GameObject.FindGameObjectsWithTag("Anchor").First();
+        Destroy(this, destoryAfter);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (randomNumber == 0){
-        transform.RotateAround(stick.transform.position, Vector3.back, rotSpeed * Time.deltaTime);
+        if (direction == "Right"){
+        transform.RotateAround(this.transform.position, Vector3.back, rotSpeed * Time.deltaTime);
         transform.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
         Debug.Log("normal right throw");
         }
-        else{
-        transform.RotateAround(stick.transform.position, Vector3.forward, rotSpeed * Time.deltaTime);
+        else if(direction == "Left"){
+        transform.RotateAround(this.transform.position, Vector3.forward, rotSpeed * Time.deltaTime);
         //transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
         transform.RotateAround(anchorForRot.transform.position, Vector3.forward, arcSpeed * Time.deltaTime);
         Debug.Log("arc left throw");
